989b8bdc615e848c9dcc303e7e62d8da6f09249f
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-06-18 20:05:30 +0500

    update neovim packages

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -75,7 +75,8 @@ return {
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
-			require("telescope").setup({
+			local telescope = require("telescope")
+			telescope.setup({
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
@@ -83,7 +84,7 @@ return {
 				},
 			})
 
-			require("telescope").load_extension("ui-select")
+			telescope.load_extension("ui-select")
 		end,
 	},
 }
