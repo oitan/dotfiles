821255f8d71aa749086c96d385a64a066d94c30b
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-06-18 16:27:03 +0500

    add eslint code actions

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -13,6 +13,7 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
       },
       on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
