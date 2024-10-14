71e5dc2eb8a81574a9900f29715a2c842136ca44
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-10-14 11:35:25 +0500

    fix prettier and eslint issues

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,70 +1,76 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "html",
-          "cssls",
-          "bashls",
-        },
-      })
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    config = function()
-      local mason_tool_installer = require("mason-tool-installer")
-      mason_tool_installer.setup({
-        lazy = true,
-        ensure_installed = {
-          "prettier",
-          "stylua",
-          "eslint",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"html",
+					"cssls",
+					"bashls",
+				},
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			local mason_tool_installer = require("mason-tool-installer")
+			mason_tool_installer.setup({
+				lazy = true,
+				ensure_installed = {
+					"prettierd",
+					"stylua",
+					"eslint_d",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      local default_ls_config = {
-        capabilities = capabilities,
-      }
+			local default_ls_config = {
+				capabilities = capabilities,
+			}
 
-      lspconfig.lua_ls.setup(default_ls_config)
-      lspconfig.tsserver.setup(default_ls_config)
-      lspconfig.html.setup(default_ls_config)
-      lspconfig.cssls.setup(default_ls_config)
-      lspconfig.bashls.setup({ unpack(default_ls_config), filetypes = { "sh", "zsh" } })
+			lspconfig.lua_ls.setup(default_ls_config)
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+				on_attach = function(client, _bufnr)
+					-- Disable tsserver's formatter if you want to use prettierd
+					client.server_capabilities.documentFormattingProvider = false
+				end,
+			})
+			lspconfig.html.setup(default_ls_config)
+			lspconfig.cssls.setup(default_ls_config)
+			lspconfig.bashls.setup({ unpack(default_ls_config), filetypes = { "sh", "zsh" } })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "see definition" })
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "see references" })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
-      vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
-      vim.keymap.set("x", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "diagnostics" })
-      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "rename" })
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "see definition" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "see references" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+			vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+			vim.keymap.set("x", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "diagnostics" })
+			vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "rename" })
+		end,
+	},
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -9,9 +9,9 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint"),
-        require("none-ls.code_actions.eslint"),
+        null_ls.builtins.formatting.prettierd,
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.code_actions.refactoring,
       },
@@ -21,5 +21,14 @@ return {
     })
 
     vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "format" })
+
+    -- Restart prettierd on prettier config changes because of prettierd caching bug
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      group = vim.api.nvim_create_augroup("RestartPrettierd", { clear = true }),
+      pattern = "*prettier*",
+      callback = function()
+        vim.fn.system("prettierd restart")
+      end,
+    })
   end,
 }
