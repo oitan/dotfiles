849f621594fdf987ffc5ad3db966ea1dc926ff7c
Alikhan Oitan <alikhan.oitan@gmail.com>
2025-09-12 16:38:37 +0500

    add new brew casks, update ghostty config, update neovim setup

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -2,7 +2,6 @@ return {
 	"rmagatti/auto-session",
 	config = function()
 		local auto_session = require("auto-session")
-		local session_lens = require("auto-session.session-lens")
 
 		auto_session.setup({
 			pre_save_cmds = { "tabdo Neotree close" },
@@ -10,6 +9,6 @@ return {
 			session_lens = {},
 		})
 
-		vim.keymap.set("n", "<leader>ls", session_lens.search_session, { noremap = true, desc = "search sessions" })
+		vim.keymap.set("n", "<leader>ls", "<cmd>AutoSession search<CR>", { noremap = true, desc = "search sessions" })
 	end,
 }
