4923f7c3cd97f7850014577aa85cc89780171afa
Alikhan Oitan <alikhan.oitan@gmail.com>
2025-10-01 12:03:56 +0500

    whitespace

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,97 +1,97 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"html",
-					"cssls",
-					"bashls",
-					"prismals",
-				},
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		config = function()
-			local mason_tool_installer = require("mason-tool-installer")
-			mason_tool_installer.setup({
-				lazy = true,
-				ensure_installed = {
-					"prettierd",
-					"stylua",
-					"eslint_d",
-					"lua_ls",
-				},
-			})
-		end,
-	},
-	{
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "html",
+          "cssls",
+          "bashls",
+          "prismals",
+        },
+      })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      local mason_tool_installer = require("mason-tool-installer")
+      mason_tool_installer.setup({
+        lazy = true,
+        ensure_installed = {
+          "prettierd",
+          "stylua",
+          "eslint_d",
+          "lua_ls",
+        },
+      })
+    end,
+  },
+  {
 
-		"neovim/nvim-lspconfig",
-		config = function()
-			local telescope = require("telescope.builtin")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    "neovim/nvim-lspconfig",
+    config = function()
+      local telescope = require("telescope.builtin")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local function with_defaults(extra)
-				return vim.tbl_deep_extend("force", { capabilities = capabilities }, extra or {})
-			end
+      local function with_defaults(extra)
+        return vim.tbl_deep_extend("force", { capabilities = capabilities }, extra or {})
+      end
 
-			local function file_diagnostics()
-				telescope.diagnostics({ bufnr = 0 })
-			end
+      local function file_diagnostics()
+        telescope.diagnostics({ bufnr = 0 })
+      end
 
-			-- define/extend configs
-			vim.lsp.config("lua_ls", with_defaults())
-			vim.lsp.config(
-				"ts_ls",
-				with_defaults({
-					on_attach = function(client, _)
-						client.server_capabilities.documentFormattingProvider = false
-					end,
-				})
-			)
-			vim.lsp.config("html", with_defaults())
-			vim.lsp.config("cssls", with_defaults())
-			vim.lsp.config("bashls", with_defaults({ filetypes = { "zsh", "sh" } }))
-			vim.lsp.config("prismals", with_defaults())
+      -- define/extend configs
+      vim.lsp.config("lua_ls", with_defaults())
+      vim.lsp.config(
+        "ts_ls",
+        with_defaults({
+          on_attach = function(client, _)
+            client.server_capabilities.documentFormattingProvider = false
+          end,
+        })
+      )
+      vim.lsp.config("html", with_defaults())
+      vim.lsp.config("cssls", with_defaults())
+      vim.lsp.config("bashls", with_defaults({ filetypes = { "zsh", "sh" } }))
+      vim.lsp.config("prismals", with_defaults())
 
-			-- enable configs (activates for their filetypes)
-			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("ts_ls")
-			vim.lsp.enable("html")
-			vim.lsp.enable("cssls")
-			vim.lsp.enable("bashls")
-			vim.lsp.enable("prismals")
+      -- enable configs (activates for their filetypes)
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("html")
+      vim.lsp.enable("cssls")
+      vim.lsp.enable("bashls")
+      vim.lsp.enable("prismals")
 
-			-- keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "see definition" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
-			vim.keymap.set("n", "gR", vim.lsp.buf.references, { desc = "see references" })
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
-			vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
-			vim.keymap.set("x", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "diagnostics" })
-			vim.keymap.set("n", "<leader>fd", file_diagnostics, { desc = "file diagnostics" })
-			vim.keymap.set("n", "<leader>fD", telescope.diagnostics, { desc = "workspace diagnostics" })
-			vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "rename" })
-		end,
-	},
+      -- keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "see definition" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
+      vim.keymap.set("n", "gR", vim.lsp.buf.references, { desc = "see references" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+      vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+      vim.keymap.set("x", "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "diagnostics" })
+      vim.keymap.set("n", "<leader>fd", file_diagnostics, { desc = "file diagnostics" })
+      vim.keymap.set("n", "<leader>fD", telescope.diagnostics, { desc = "workspace diagnostics" })
+      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "rename" })
+    end,
+  },
 }
